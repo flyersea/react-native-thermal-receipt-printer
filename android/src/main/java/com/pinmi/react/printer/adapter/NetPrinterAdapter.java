dd566b7e97df8db13165ffe3966092bfa983be99
Harry <harry@coolnology.com>
2022-06-28 16:04:17 +1000

    Bug fixes.

--- android/src/main/java/com/pinmi/react/printer/adapter/NetPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/NetPrinterAdapter.java
@@ -250,6 +250,31 @@ public class NetPrinterAdapter implements PrinterAdapter {
 
     }
 
+    @Override
+    public void connectAndSend(String host, Integer port, String rawBase64Data, Callback successCallback, Callback errorCallback)  {
+      try {
+          byte[] data = Base64.decode(rawBase64Data, Base64.DEFAULT);
+
+          Socket socket = new Socket();
+          socket.connect(new InetSocketAddress(host, port), 1500);
+          OutputStream output = socket.getOutputStream();
+          output.write(data);
+          output.close();
+          socket.close();
+          successCallback.invoke();
+      } catch(IOException e) {
+                            Log.e(LOG_TAG, "bello :: int cause to print data" + e.toString());
+                    e.printStackTrace();
+                errorCallback.invoke(e.toString());
+
+        // throw e;
+      }
+    }
+
+    @Override
+    public void connectAndSend(PrinterDeviceId printerDeviceId, String data, Callback successCallback, Callback errorCallback) {
+    }
+
     public static Bitmap getBitmapFromURL(String src) {
         try {
             URL url = new URL(src);
