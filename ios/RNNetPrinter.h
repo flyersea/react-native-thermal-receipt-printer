360f89a5870b258a197aa72fae21804736a6858d
Harry <harry@coolnology.com>
2022-06-30 00:53:01 +1000

    Bug fixes.

--- ios/RNNetPrinter.h
+++ ios/RNNetPrinter.h
@@ -3,11 +3,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-@interface RNNetPrinter : RCTEventEmitter <RCTBridgeModule>{
-    NSString *connected_ip;
-    NSString *current_scan_ip;
-    NSMutableArray* _printerArray;
-    bool is_scanning;
+@interface RNNetPrinter : NSObject <RCTBridgeModule>{
 }
 
 @end
